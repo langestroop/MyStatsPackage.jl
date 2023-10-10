0766e196f20ae94652e81607ece00914bbd21bad
langestroop <alberto.mariola@icloud.com>
2023-10-10 15:29:12 +0200

    fixing .github folder permission issue

--- src/statistic_functions.jl
+++ src/statistic_functions.jl
@@ -2,10 +2,10 @@
 using ProgressMeter
 import Base: length
 function rse_sum(x)
-    @assert all(!isnan,x) "Watch out, there might be NaNs"
+    @assert all(!isnan, x) "Watch out, there might be NaNs"
     s = 0
     @showprogress for k = eachindex(x)
-        s = s+x[k]
+        s = s + x[k]
     end
     return s
 end
@@ -21,13 +21,13 @@ rse_mean(-15:17) == 1
 
 #----
 function rse_std(x)
-   return  sqrt(rse_sum((x.-rse_mean(x)).^2)/(length(x)-1))
+    return sqrt(rse_sum((x .- rse_mean(x)) .^ 2) / (length(x) - 1))
 end
-rse_std([1,2,3]) == 1
+rse_std([1, 2, 3]) == 1
 
 #----
-function rse_tstat(x;σ = rse_std(x))
-    return rse_mean(x)./ (σ / sqrt(length(x)))
+function rse_tstat(x; σ=rse_std(x))
+    return rse_mean(x) ./ (σ / sqrt(length(x)))
 end
 
 rse_tstat(2:3) == 5
@@ -41,16 +41,16 @@ struct StatResult
     tvalue::Float64
 end
 Base.length(s::StatResult) = s.n
-StatResult(x) = StatResult(x,length(x))
+StatResult(x) = StatResult(x, length(x))
 
-StatResult(x,n) =  StatResult(x,n,rse_std(x))
-StatResult(x,n,std) = StatResult(x,n,std,rse_tstat(x;σ=std))
+StatResult(x, n) = StatResult(x, n, rse_std(x))
+StatResult(x, n, std) = StatResult(x, n, std, rse_tstat(x; σ=std))
 
 
-StatResult([10,500.]) # <1>
+StatResult([10, 500.0]) # <1>
 
 function tstat(x)
-    return StatResult(length(x),rse_tstat(x))
+    return StatResult(length(x), rse_tstat(x))
 end
 
 #---
@@ -58,7 +58,7 @@ end
 
 function printOwner()
 
-    x = print("Welcome, this is the first test pakcage by: langestroop");
+    x = print("Welcome, this is the first test pakcage by: langestroop")
     return x
 
-end 
+end
